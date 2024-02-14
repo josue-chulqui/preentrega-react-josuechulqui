f5075755c196e454d7fefed09377bc318676fdf9
josue chulqui <josue.c1095@gmail.com>
2024-02-14 14:37:18 -0300

    joya

---
+++ ecomerce1/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar =() => {
+    return (
+        <nav>
+            <h3>Ecomerce</h3>
+            <div>
+                <button>Remeras</button>
+                <button>Pantalones</button>
+                <button>Calzados</button>
+                <button>Buzos</button>
+            </div>
+        </nav>
+    )
+}
+export default NavBar
